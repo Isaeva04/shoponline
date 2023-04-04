fd114806d4c603e55992711e387d6b1b5c5399a2
-ASUS- <Isaevaa@tut.by>
2023-04-05 00:43:22 +0300

    shop

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///shop.db'  # файл в котором будет храниться база данных
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary_key автоматическое проставление порядкового номера
+    title = db.Column(db.String(100),nullable=False)
+    price =db.Column(db.Integer,nullable=False)
+    isActive = db.Column(db.Boolean, default=True)  # активный товар-есть в наличии. True проставляется автоматически
+    #text = db.Column(db.Text, nullable=False)
 
 @app.route('/')
 def index():
@@ -16,7 +25,11 @@ def about():
 def contacts():
     return render_template('contacts.html')
 
-
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
