c91cfd9f8ae99892a76ede98112296e71700c15b
-ASUS- <Isaevaa@tut.by>
2023-04-05 23:39:42 +0300

    shop

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template, request,redirect
 from flask_sqlalchemy import SQLAlchemy
+from cloudipsp import Api, Checkout
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///shop.db'  # файл в котором будет храниться база данных
@@ -43,6 +44,19 @@ def create():
             return "Произошла ошибка"
     else:
         return render_template('create.html')
+
+@app.route('/buy/<int:id>')
+def buy(id):
+    item = Item.query.get(id)
+    api = Api(merchant_id=1396424,
+              secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+        "currency": "BYN",
+        "amount": item.price
+    }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
 
 if __name__ == '__main__':
     with app.app_context():
